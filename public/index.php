7c566ca27308636f645c72efe539b67c0389de3a
Felipe Mendonça <felipe.mendonca@novam3.com>
2020-06-12 16:36:40 -0300

    🎉

---
+++ public/index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title>Autovema</title>
+    <link rel="stylesheet" href="css/app.css" />
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+  </head>
+  <body>
+   <div class="welcome-site">
+  <nav class="navbar navbar-expand-lg navbar-light bg-light d-flex justify-content-center p-3 pb-4">
+    <a class="navbar-brand" href="#">
+      <img src="../images/logo.svg" alt="">
+    </a>
+  </nav>
+	<div class="d-flex flex-column flex-lg-row">
+		<div class="bg-site bg-fiat">
+			<div class="logo-princial">
+				<a href="http://www.autovemafiat.com.br/">
+					<img src="images/fiat.png" alt="" class="img-fluid img-only" style="margin-top: -24px;">
+				</a>	
+      </div>
+      <div class="link d-flex justify-content-center">
+        <div>
+          <a href="http://www.autovemafiat.com.br/" class=" btn-fiat text-uppercase">ir para <strong>autovemafiat.com.br</strong><img src="images/arrow-right.svg" alt=""></a>
+        </div>
+      </div>
+      <img src="images/BFiat.png" alt="" class="img-fluid bg-marca">
+		</div>
+		<div class="bg-site bg-mitsubshi">
+			<div class="logo-princial">
+				<a href="http://www.autovemamotors.com.br/">
+					<img src="images/mitsubshi.png" alt="" class="img-fluid img-only">
+				</a>	
+      </div>
+      <div class="link d-flex justify-content-center">
+        <div>
+          <a href="http://www.autovemamotors.com.br/" class="btn-mitsubshi text-uppercase">ir para <strong>autovemamotors.com.br</strong><img src="images/arrow-right.svg" alt=""></a>
+        </div>
+      </div>
+      <img src="images/Mit.png" alt="" class="img-fluid bg-marca">
+		</div>
+	</div>
+  </body>
+</html>
